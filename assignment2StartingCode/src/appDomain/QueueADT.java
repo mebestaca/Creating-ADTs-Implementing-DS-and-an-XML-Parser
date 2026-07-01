18a75c951bb34f7d04ccc2e5685df4b071afe90f
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-30 21:10:25 -0600

    added queueadt

--- assignment2StartingCode/src/appDomain/QueueADT.java
+++ assignment2StartingCode/src/appDomain/QueueADT.java
@@ -1,5 +1,32 @@
 package appDomain;
 
-public class QueueADT {
+import utilities.Iterator;
 
+public interface QueueADT<E> {
+
+    void enqueue(E element);
+
+    E dequeue();
+
+    E peek();
+
+    void dequeueAll();
+
+    boolean contains(E element);
+
+    boolean equals(QueueADT<E> that);
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    int size();
+
+    int search(E element);
+
+    Object[] toArray();
+
+    E[] toArray(E[] array);
+
+    Iterator<E> iterator();
 }
