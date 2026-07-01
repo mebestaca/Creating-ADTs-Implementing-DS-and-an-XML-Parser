afea989cbb409b28fd02b8bd8db410f2e367f8b0
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-30 21:10:58 -0600

    added stackadt

--- assignment2StartingCode/src/appDomain/StackADT.java
+++ assignment2StartingCode/src/appDomain/StackADT.java
@@ -1,5 +1,32 @@
 package appDomain;
 
-public class StackADT {
+import utilities.Iterator;
 
+public interface StackADT<E> {
+
+    void push(E element);
+
+    E pop();
+
+    E peek();
+
+    void clear();
+
+    boolean contains(E element);
+
+    boolean equals(StackADT<E> that);
+
+    boolean isEmpty();
+
+    int size();
+
+    int search(E element);
+
+    boolean stackOverflow();
+
+    Object[] toArray();
+
+    E[] toArray(E[] array);
+
+    Iterator<E> iterator();
 }
