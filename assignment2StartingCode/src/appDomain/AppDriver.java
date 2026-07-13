d7c39758be80a11a409ba9cc22e7ed101c8eb09b
Marc Edison Estaca <mebestaca@gmail.com>
2026-07-12 21:10:16 -0600

    setting up xmlparser

---
+++ assignment2StartingCode/src/appDomain/AppDriver.java
@@ -0,0 +1,14 @@
+package appDomain;
+
+public class AppDriver {
+	public static void main( String[] args )
+	{
+		
+		if (args.length != 1) {
+	        System.out.println("Usage: java -jar Parser.jar <xml-file>");
+	        return;
+	    }
+		
+		
+	}
+}
